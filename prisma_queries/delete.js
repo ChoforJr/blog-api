f13ea798b32eedc16759fbec4187a865f8895588
ChoforJr <choforforsakang@gmail.com>
2025-11-27 23:18:56 +0100

    created the queries and functions for deleting user

--- prisma_queries/delete.js
+++ prisma_queries/delete.js
@@ -1,24 +1,12 @@
 import prisma from "../config/prisma.js";
 
-// export async function delFolder(id) {
-//   await prisma.folders.delete({
-//     where: {
-//       id: id,
-//     },
-//   });
-// }
-
-// export async function delFile(id) {
-//   await prisma.files.delete({
-//     where: {
-//       id: id,
-//     },
-//   });
-// }
-
-// export async function deleteAllUsers() {
-//   await prisma.user.deleteMany({});
-// }
+export async function deleteAllUsersExceptAdmin() {
+  await prisma.user.deleteMany({
+    where: {
+      role: "USER",
+    },
+  });
+}
 
 export async function deleteUserByID(userID) {
   await prisma.user.delete({
