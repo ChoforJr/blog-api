37ee4aebab236834d66d2efdbabdd64e52e38468
ChoforJr <choforforsakang@gmail.com>
2025-12-01 01:17:51 +0100

    connect the adminRouter to controller post delete functions

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -9,6 +9,11 @@ import {
   checkPostValidationResult,
 } from "../validations/validatePost.js";
 import { readPosts } from "../controllers/read.js";
+import {
+  removePost,
+  removeAllPosts,
+  removeAllDraftedPosts,
+} from "../controllers/delete.js";
 import postRouter from "./postRouter.js";
 
 const adminRouter = Router();
@@ -19,7 +24,12 @@ adminRouter.get("/users", readUsers);
 adminRouter.delete("/user/:id", removeUserByAdmin);
 adminRouter.delete("/users", removeAllUsersByAdmin);
 
-postRouter.get("/post/all", readPosts);
+adminRouter.get("/post/all", readPosts);
+
+adminRouter.delete("/post/all", removeAllPosts);
+adminRouter.delete("/post/drafted", removeAllDraftedPosts);
+adminRouter.delete("/post/:id", removePost);
+
 adminRouter.use(
   "/post",
   validatePostRules,
