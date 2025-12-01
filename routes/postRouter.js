f6cf128b4fca32424d607c9af9d631d107ae4257
ChoforJr <choforforsakang@gmail.com>
2025-12-01 01:09:02 +0100

    Connect the post router to the put controller function

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -3,10 +3,14 @@ import {
   addAndPublishNewPost,
   addAndDraftNewPost,
 } from "../controllers/post.js";
+import { editAndPublishPost, editAndDraftPost } from "../controllers/put.js";
 
 const postRouter = Router();
 
 postRouter.post("/publish", addAndPublishNewPost);
 postRouter.post("/draft", addAndDraftNewPost);
+
+postRouter.put("/publish", editAndPublishPost);
+postRouter.put("/draft", editAndDraftPost);
 
 export default postRouter;
